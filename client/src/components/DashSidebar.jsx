704896ccb46f58eb1f5191b614eee0a9a9ac6802
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-24 21:54:46 +0500

    added DashSidebar to the Dashboard page

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  Sidebar,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
+import { CgProfile } from "react-icons/cg";
+import { FaSignOutAlt } from "react-icons/fa";
 
 export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
+    }
+  }, [location.search]);
   return (
-    <div>DashSidebar</div>
-  )
+    <Sidebar className="w-full md:w-56">
+      <SidebarItems>
+        <SidebarItemGroup>
+          <SidebarItem
+            active={tab == "profile"}
+            label={"User"}
+            labelColor="dark"
+            icon={CgProfile}
+          >
+            Profile
+          </SidebarItem>
+          <SidebarItem icon={FaSignOutAlt}>Sign Out</SidebarItem>
+        </SidebarItemGroup>
+      </SidebarItems>
+    </Sidebar>
+  );
 }

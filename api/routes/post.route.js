b045756f104255cdbbd8817bb7cd9a7997527f37
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-30 01:29:53 +0500

    get instead of post

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -3,5 +3,5 @@ import { verifyToken } from '../utilis/verifyToken.js';
 import { create ,getPosts} from '../controllers/post.controller.js';
 const router=express.Router();
 router.post("/create",verifyToken,create);
-router.post("/getposts",getPosts);
+router.get("/getposts",getPosts);
 export default router;

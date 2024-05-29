23ab2136a6f48d8508bfd0bb200ced40fcbe4067
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-30 01:28:24 +0500

    create getPost Api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utilis/verifyToken.js';
-import { create } from '../controllers/post.controller.js';
+import { create ,getPosts} from '../controllers/post.controller.js';
 const router=express.Router();
-router.post("/create",verifyToken,create)
+router.post("/create",verifyToken,create);
+router.post("/getposts",getPosts);
 export default router;

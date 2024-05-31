a383031e10942672bd8684b5761d508c68ff3ff4
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-31 21:44:35 +0500

    created get users api route and add its name on the dashboard page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,13 +3,15 @@ import {
   test,
   updateUser,
   deleteUser,
-  signOut
+  signOut,
+  getUsers
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utilis/verifyToken.js";
 const router = express.Router();
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout",signOut)
+router.post("/signout",signOut);
+router.get("/getusers",verifyToken,getUsers);
 
 export default router;

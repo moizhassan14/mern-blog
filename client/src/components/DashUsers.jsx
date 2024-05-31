72f37b3d646c1bae9fdfd3c94368e50245bbfa0d
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-31 22:30:08 +0500

    created the delete user functionality but i will check this tomorrow

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { FaCheck, FaTimes } from "react-icons/fa";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 export default function DashUsers() {
   const { currentUser } = useSelector((state) => state.user);
   const [users, setUsers] = useState([]);
@@ -32,9 +31,7 @@ export default function DashUsers() {
   const handleShowMore = async () => {
     const startIndex = users.length;
     try {
-      const res = await fetch(
-        `/api/user/getusers?startIndex=${startIndex}`
-      );
+      const res = await fetch(`/api/user/getusers?startIndex=${startIndex}`);
       const data = await res.json();
       if (res.ok) {
         setUsers((prev) => [...prev, ...data.users]);
@@ -47,6 +44,20 @@ export default function DashUsers() {
     }
   };
   const handleDeleteUser = async () => {
+    try {
+      const res =await fetch(`/api/user/delete/${userToDelete}`,{
+        method:"DELETE"
+      });
+      const data=await res.json();
+      if(!res.ok){
+        console.log(data.message);
+      }else{
+        setUsers((prev)=>prev.filter((user)=>user._id !== userToDelete));
+        setOpenModal(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -68,21 +79,19 @@ export default function DashUsers() {
                     {new Date(user.createdAt).toLocaleDateString()}
                   </Table.Cell>
                   <Table.Cell>
-                      <img
-                        src={user.profilePicture}
-                        alt={user.username}
-                        className="w-10 h-10 object-cover bg-gray-500 rounded-full"
-                      />
+                    <img
+                      src={user.profilePicture}
+                      alt={user.username}
+                      className="w-10 h-10 object-cover bg-gray-500 rounded-full"
+                    />
                   </Table.Cell>
-                  <Table.Cell>
-                      {user.username}
-                  </Table.Cell>
+                  <Table.Cell>{user.username}</Table.Cell>
                   <Table.Cell>{user.email}</Table.Cell>
                   <Table.Cell>
                     {user.isAdmin ? (
-                      <FaCheck className='text-green-500' />
+                      <FaCheck className="text-green-500" />
                     ) : (
-                      <FaTimes className='text-red-500' />
+                      <FaTimes className="text-red-500" />
                     )}
                   </Table.Cell>
                   <Table.Cell>
@@ -122,7 +131,7 @@ export default function DashUsers() {
             </h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={handleDeleteUser}>
-              Yes, I&apos;m sure.
+                Yes, I&apos;m sure.
               </Button>
               <Button color="gray" onClick={() => setOpenModal(false)}>
                 No, cancel

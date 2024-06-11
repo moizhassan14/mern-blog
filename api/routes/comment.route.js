63c0de8d9f1bfb921abfb56c5d4c467dcc043718
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-06-11 19:44:08 +0500

    completed edit functionality in the comment of the post page

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,8 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utilis/verifyToken.js";
-import { createCommment , getPostComments ,likeComment } from "../controllers/comment.controller.js";
+import {
+  createCommment,
+  getPostComments,
+  likeComment,
+  editComment,
+} from "../controllers/comment.controller.js";
 const router = express.Router();
 router.post("/create", verifyToken, createCommment);
-router.get("/getPostComments/:postId",getPostComments);
-router.put("/likeComment/:commentId",verifyToken,likeComment)
+router.get("/getPostComments/:postId", getPostComments);
+router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 export default router;

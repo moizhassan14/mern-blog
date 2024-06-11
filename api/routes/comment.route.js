311153c6e3fe3f5fdd680002d1382703bad08c89
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-06-11 20:21:54 +0500

    completed delete functionality in the comment of the post page

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -5,10 +5,12 @@ import {
   getPostComments,
   likeComment,
   editComment,
+  deleteComment
 } from "../controllers/comment.controller.js";
 const router = express.Router();
 router.post("/create", verifyToken, createCommment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 router.put("/editComment/:commentId", verifyToken, editComment);
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
 export default router;
